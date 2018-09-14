f1d9bea42f6ff6bec8d40ad78595606127467191
James <jamesegconnor@gmail.com>
2018-09-14 12:28:47 -0400

    Created start of home screen code.

---
+++ Chrysalis/src/chrysalis/homeScreen.java
@@ -0,0 +1,22 @@
+package chrysalis;
+
+//Imports
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class homeScreen extends Applet
+{
+    //Entry point for the program.
+    public static void main(String[] args)
+    {
+        //Initialize the home screen.
+        homeScreen hs = new homeScreen();
+    }
+    
+    //Draw the graphics.
+    @Override
+    public void paint(Graphics g)
+    {
+        
+    }
+}
