61e0700fb473f1d977fc219d0ab8272dd9fc69ca
James <jamesegconnor@gmail.com>
2018-09-19 09:46:30 -0400

    Updated Screen System
    - Screens are now based off of a common interface.
    - Screens now have names.
    - You can change screens by calling changeScreen in handler with either the instance or the instance's name.
    - Created a template for new screens. Copy and fill out the correct information.

--- Chrysalis/src/chrysalis/handler.java
+++ Chrysalis/src/chrysalis/handler.java
@@ -1,42 +1,65 @@
 package chrysalis;
 
 import java.applet.Applet;
-import java.awt.Component;
-import java.awt.Graphics;
+import java.awt.Dimension;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class handler extends Applet
 {
-    //Screens Available
-    public static enum screen
-    {
-        home,
-        createCommit,
-        approveCommit,
-        viewCommits,
-        createUser
-    }
+    //Dictionary of all screens, linking them with their names.
+    public static Dictionary<String, screen> screens = new Hashtable<>();
     
-    screen currScreen = screen.home;
+    //The current screen the user is looking at.
+    screen currScreen;
     
-    homeScreen home;
-    
+    //Initialization method called by applet.
     @Override
     public void init()
     {
-        home = new homeScreen(this);
-        changeComponents(home);
+        //Create a new Home Screen. When it's created, it will automatically register itself.
+        new homeScreen(this);
+        new TestScreen(this);
     }
     
-    @Override
-    public void paint(Graphics g)
+    //Change the screen by adding the component (screen is a JPanel).
+    public void changeScreen(screen s)
     {
-        super.paint(g);
-    }
-    
-    public void changeComponents(Component panel)
-    {
-        this.removeAll();
+        this.removeAll();       
+        this.add(s);
         
-        this.add(panel);
+        this.repaint();
+    }
+    
+    //Change the screen by adding the component, based off of it's name (screen is a JPanel).
+    public void changeScreen(String s)
+    {
+        this.removeAll();  
+        //Add it if the screen exists.
+        this.add(screens.get(s));
+        
+        //Basically repaints it so the graphics all show up properly.
+        //TODO Make this not so hacky and bad.
+        Dimension d = this.getSize();
+        this.resize(d.width - 1, d.height - 1);
+        d = this.getSize();
+        this.resize(d.width + 1, d.height + 1);
+    }
+    
+    //Register a screen. This is called by every new screen, and adds it to screens.
+    public void registerScreen(screen s, String name)
+    {
+        //Add screen to screens.
+        screens.put(name, s);
+        
+        //If it's the first screen, it should be the one that we load first.
+        if(screens.size() == 1)
+        {
+            //Set the current screen.
+            currScreen = s;
+            
+            //Change it to that screen.
+            this.changeScreen(currScreen);
+        }
     }
 }
--- Chrysalis/src/chrysalis/homeScreen.java
+++ Chrysalis/src/chrysalis/homeScreen.java
@@ -1,13 +1,13 @@
 package chrysalis;
 
 //Imports
-import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class homeScreen extends JPanel implements ActionListener
+//Represents home screen.
+public class homeScreen extends screen
 {   
+    //Stores the handler for changing screens.
     handler h;
     
     
@@ -18,45 +18,44 @@ public class homeScreen extends JPanel implements ActionListener
     //--------------------------
     
     
+    //Constructor for Home Screen. Passes handler to screen constructor and sets h variable to handler.
+    //These are used for changing screens and registering this screen.
     public homeScreen(handler _h)
     {
+        super(_h, "Home");        
         h = _h;
-        
-        this.initialize();
     }
     
+    //A required initialization method that creates all of the components.
+    @Override
     public void initialize()
     {
+        //Make the createCommit button and store it to a variable.
         createCommit = new JButton("Create Commit");
         createCommit.addActionListener(this);
         this.add(createCommit);
         
+        //Make the approveCommit button and store it to a variable.
         approveCommit = new JButton("Approve Commit");
         approveCommit.addActionListener(this);
         this.add(approveCommit);
         
+        //Make the createUser button and store it to a variable.
         createUser = new JButton("Create User");
         createUser.addActionListener(this);
         this.add(createUser);
-        
-        this.setVisible(true);
     }
     
-    //Draw the graphics.
-    @Override
-    public void paintComponent(Graphics g)
-    {        
-        super.paintComponent(g);
-    }
-    
+    //Implementation of required actionPerformed method. This handles button events.
     @Override
     public void actionPerformed(ActionEvent e)
     {
+        //Change screens according to different buttons.
         if(e.getSource() == createCommit)
-            h.changeComponents(this);
+            h.changeScreen("Test");
         else if(e.getSource() == approveCommit)
-            h.changeComponents(this);
+            h.changeScreen(this);
         else if(e.getSource() == createUser)
-            h.changeComponents(this);
+            h.changeScreen(this);
     }
 }
